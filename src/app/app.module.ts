fd5a09e6e2c568bc694771383d6afbc16c166928
Sandesh <psandesh01.sp@gmail.com>
2021-11-06 16:52:35 +0530

    Added portfolio table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,19 @@ import { ChartsModule } from 'ng2-charts';
 import { AppComponent } from "./app.component";
 import { FormsModule } from '@angular/forms';
 import { NgApexchartsModule } from "ng-apexcharts";
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { MaterialModule } from './material/material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, ChartsModule ,FormsModule, NgApexchartsModule],
+  declarations: [AppComponent, PortfolioComponent],
+  imports: [
+    BrowserModule,
+    ChartsModule,
+    FormsModule, NgApexchartsModule, BrowserAnimationsModule, MatTableModule, MaterialModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
